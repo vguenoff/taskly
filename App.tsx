5551a4db83a3f339be940dc9a409b367963c5bf5
Vasil Genov <vguenoff@gmail.com>
2024-12-14 12:33:38 +0200

    initial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View, StyleSheet } from 'react-native'
+import theme from './theme'
+
+// Cmd + D in the simulator open dev menu
+// Shift + M in the terminal open dev menu
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.textWrapper}>
+        <Text style={styles.text}>Coffee</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: theme.color.white,
     justifyContent: 'center',
   },
-});
+  textWrapper: {
+    borderBottomWidth: 1,
+    borderBottomColor: theme.color.cerulean,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 200,
+  },
+})
---
+++ theme.ts
@@ -0,0 +1,8 @@
+const theme = {
+  color: {
+    cerulean: '#1a759f',
+    white: '#fff',
+  },
+}
+
+export default theme
